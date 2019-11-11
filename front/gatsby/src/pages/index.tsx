ff3480d914329dbe9a44b6188095465fdec76137
YJ <yjkimjunior@gmail.com>
2019-11-11 18:03:34 +0100

    feat: add ui-common, uic-omponents

--- front/gatsby/src/pages/index.tsx
+++ front/gatsby/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { NavButton } from '@substrate/ui-components/src';
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -8,7 +9,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <NavButton>Nav button</NavButton>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
