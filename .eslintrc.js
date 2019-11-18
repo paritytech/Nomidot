a4b0a7d10f268b7cb6941232efdb98574ac9940a
YJ <yjkimjunior@gmail.com>
2019-11-18 13:02:09 +0100

    chore: eslint-prettier-plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'semistandard',
+    'plugin:prettier/recommended',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
