fe402eb8cd470c765f975c1890348b23b2d98c4f
YJ <yjkimjunior@gmail.com>
2019-12-03 12:46:31 +0100

    fix: tsconfig

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+const config = require('@polkadot/dev/config/jest');
+
+module.exports = Object.assign({}, config);
