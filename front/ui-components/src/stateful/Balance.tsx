b98b96de323662f8bcaea988aeffab851b73c6e9
YJ <yjkimjunior@gmail.com>
2020-01-02 18:24:58 +0100

    fix: make tsc emit properly (#56)

--- front/ui-components/src/stateful/Balance.tsx
+++ front/ui-components/src/stateful/Balance.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { DerivedBalances, DerivedStakingAccount } from '@polkadot/api-derive/types';
+// import { DerivedBalances, DerivedStakingAccount } from '@polkadot/api-derive/types';
 import { ApiContext } from '@substrate/context';
 import React, { useContext, useEffect, useState } from 'react';
 import { combineLatest, of } from 'rxjs';
