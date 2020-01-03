394a529fd39a790c8581ca3b63bd5d60ac680761
YJ <yjkimjunior@gmail.com>
2020-01-03 12:45:59 +0100

    fix: import path

--- front/ui-components/src/WalletCard.tsx
+++ front/ui-components/src/WalletCard.tsx
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Card, Header, Stacked, SubHeader, WithSpaceAround } from '@substrate/ui-components';
+import { Card, Header, Stacked, SubHeader, WithSpaceAround } from './index';
 import React from 'react';
 
 type WalletCardProps = {
