df4d415252e8035267d9c89c3fac9d42aa125757
YJ <yjkimjunior@gmail.com>
2020-01-30 18:39:31 +0100

    fix: make server lint

--- back/server/src/utils.ts
+++ back/server/src/utils.ts
@@ -1,5 +1,9 @@
-import { Prisma } from './generated/prisma-client'
+// Copyright 2018-2020 @paritytech/nomidot authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { Prisma } from './generated/prisma-client';
 
 export interface Context {
-  prisma: Prisma
+  prisma: Prisma;
 }
