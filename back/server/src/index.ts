79538d72be0b8c489f4af0a6a45cdb926eb89963
YJ <yjkimjunior@gmail.com>
2020-01-30 19:14:17 +0100

    fix(#77): back/server eslint errors (#124)
    * fix: make server lint
    
    * fix: whack the e2e tests

--- back/server/src/index.ts
+++ back/server/src/index.ts
@@ -1,57 +1,106 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { prisma } from './generated/prisma-client'
-import { Context } from './utils'
+// Copyright 2018-2020 @paritytech/nomidot authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { GraphQLServer } from 'graphql-yoga';
+
+import { prisma } from './generated/prisma-client';
+import { Context } from './utils';
+
+type Selectors = Record<string, any>;
 
 const resolvers = {
   Query: {
-    blockNumber(parent, { number }, context: Context) {
-      return context.prisma.blockNumber({ number })
+    blockNumber(parent: any, { number }: Selectors, context: Context) {
+      return context.prisma.blockNumber({ number });
     },
-    blockNumberAtHash(parent, { hash }, context: Context) {
-      return context.prisma.blockNumber({ hash })
+    blockNumberAtHash(parent: any, { hash }: Selectors, context: Context) {
+      return context.prisma.blockNumber({ hash });
     },
-    blockNumbersAuthoredBy(parent, { authoredBy }, context: Context) {
+    blockNumbersAuthoredBy(
+      parent: any,
+      { authoredBy }: Selectors,
+      context: Context
+    ) {
       return context.prisma.blockNumbers({ where: { authoredBy } });
     },
-    eraAtIndex(parent, { index }, context: Context) {
-      return context.prisma.era({ index })
+    eraAtIndex(parent: any, { index }: Selectors, context: Context) {
+      return context.prisma.era({ index });
     },
-    erasWhere(parent, { first, orderBy, skip }, context: Context) {
-      return context.prisma.eras({ first, orderBy, skip })
+    erasWhere(
+      parent: any,
+      { first, orderBy, skip }: Selectors,
+      context: Context
+    ) {
+      return context.prisma.eras({ first, orderBy, skip });
     },
-    nominationsWhere(parent, where, context: Context) {
-      return context.prisma.nominations({ where })
+    nominationsWhere(parent: any, where: Selectors, context: Context) {
+      return context.prisma.nominations({ where });
     },
-    sessionAtIndex(parent, { index }, context: Context) {
-      return context.prisma.session({ index })
+    sessionAtIndex(parent: any, { index }: Selectors, context: Context) {
+      return context.prisma.session({ index });
     },
-    sessionsWhere(parent, { first, orderBy, skip }, context: Context) {
-      return context.prisma.sessions({ first, orderBy, skip })
+    sessionsWhere(
+      parent: any,
+      { first, orderBy, skip }: Selectors,
+      context: Context
+    ) {
+      return context.prisma.sessions({ first, orderBy, skip });
     },
-    slashingsAtBlock(parent, { blockNumber }, context: Context) {
-      return context.prisma.slashings({ where: { blockNumber } })
+    slashingsAtBlock(
+      parent: any,
+      { blockNumber }: Selectors,
+      context: Context
+    ) {
+      return context.prisma.slashings({ where: { blockNumber } });
     },
-    slashingsWhere(parent, { first, orderBy, skip }, context: Context) {
+    slashingsWhere(
+      parent: any,
+      { first, orderBy, skip }: Selectors,
+      context: Context
+    ) {
       return context.prisma.slashings({ first, orderBy, skip });
     },
-    slashingsAtBlockWhere(parent, { blockNumber, first, orderBy, skip }, context: Context) {
-      return context.prisma.slashings({ where: { blockNumber }, first, orderBy, skip });
+    slashingsAtBlockWhere(
+      parent: any,
+      { blockNumber, first, orderBy, skip }: Selectors,
+      context: Context
+    ) {
+      return context.prisma.slashings({
+        where: { blockNumber },
+        first,
+        orderBy,
+        skip,
+      });
     },
-    validatorsAtSession(parent, { session }, context: Context) {
-      return context.prisma.validators({ where: { session } })
+    validatorsAtSession(parent: any, { session }: Selectors, context: Context) {
+      return context.prisma.validators({ where: { session } });
     },
-    validatorsWhere(parent, { first, orderBy, skip }, context: Context) {
+    validatorsWhere(
+      parent: any,
+      { first, orderBy, skip }: Selectors,
+      context: Context
+    ) {
       return context.prisma.validators({ first, orderBy, skip });
     },
-    validatorsAtSessionWhere(parent, { session, first, orderBy, skip }, context: Context) {
-      return context.prisma.validators({ where: { session }, first, orderBy, skip });
+    validatorsAtSessionWhere(
+      parent: any,
+      { session, first, orderBy, skip }: Selectors,
+      context: Context
+    ) {
+      return context.prisma.validators({
+        where: { session },
+        first,
+        orderBy,
+        skip,
+      });
     },
   },
-}
+};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
   context: { prisma },
-})
-server.start(() => console.log('Server is running on http://localhost:4000'))
+});
+server.start(() => console.log('Server is running on http://localhost:4000'));
