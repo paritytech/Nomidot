c75f76b1c425f068810f82defd61cba60d12074b
YJ <yjkimjunior@gmail.com>
2020-02-04 17:32:12 +0100

    feat: chain best head subscription

--- back/server/src/index.ts
+++ back/server/src/index.ts
@@ -2,14 +2,25 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
-import { prisma } from './generated/prisma-client';
+import { prisma, Prisma } from './generated/prisma-client';
 import { Context } from './utils';
 
 type Selectors = Record<string, any>;
 
 const resolvers = {
+  Subscription: {
+    chainBestBlockNumber: {
+      subscribe(parent: any, args, context: Context, info) {
+        return context.prisma.$subscribe.blockNumber(
+          {
+            mutation_in: ['CREATED']
+          }
+        )
+      }
+    }
+  },
   Query: {
     blockNumber(parent: any, { number }: Selectors, context: Context) {
       return context.prisma.blockNumber({ number });
@@ -96,11 +107,22 @@ const resolvers = {
       });
     },
   },
+  Mutation: {
+    writeBlockNumber(parent, { hash, number }, context, info) {
+      return context.prisma.createBlockNumber({
+        number,
+        authoredBy: 'YJKIM',
+        startDateTime: new Date(Date.now()).toISOString(),
+        hash,
+      },
+      info);
+    }
+  }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma },
+  context: { prisma }
 });
 server.start(() => console.log('Server is running on http://localhost:4000'));
