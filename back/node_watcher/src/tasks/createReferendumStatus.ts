2f81597b6c28b9d7aca2dc16f2195e0142617931
thib <thibaut.sardan@gmail.com>
2020-02-07 14:36:54 +0100

    fix

--- back/node_watcher/src/tasks/createReferendumStatus.ts
+++ back/node_watcher/src/tasks/createReferendumStatus.ts
@@ -63,7 +63,7 @@ const createReferendumStatus: Task<NomidotReferendumStatusUpdate[]> = {
         }
 
         const relatedReferendum = await prisma.referendum({
-          id: parseInt(referendumRawEvent.ReferendumIndex),
+          referendumId: parseInt(referendumRawEvent.ReferendumIndex),
         });
 
         if (!relatedReferendum) {
