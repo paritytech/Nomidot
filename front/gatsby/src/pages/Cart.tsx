ad71c1fef62b0a934c60bf83b90896bd186d7166
YJ <yjkimjunior@gmail.com>
2020-02-26 13:34:55 +0100

    fix: empty cart situation

--- front/gatsby/src/pages/Cart.tsx
+++ front/gatsby/src/pages/Cart.tsx
@@ -3,22 +3,21 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { RouteComponentProps } from '@reach/router';
-import { AddressSummary, Container, Grid, Icon, List, Margin, StackedHorizontal, WithSpaceAround } from '@substrate/ui-components';
+import { AddressSummary, Container, Grid, Icon, List, Margin, Stacked, StackedHorizontal, WithSpaceAround } from '@substrate/ui-components';
 import { Button, Subheading } from '@substrate/design-system';
+import { navigate } from 'gatsby';
 import React, { useEffect, useState } from 'react';
 
-import { getCartItems, removeCartItem, stripAddressFromCartItem } from '../util/helpers';
+import { clearCart, getCartItems, removeCartItem, stripAddressFromCartItem } from '../util/helpers';
 
 type Props = RouteComponentProps;
 
 const Cart = (_props: Props): React.ReactElement => {
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState<string[]>([]);
 
   useEffect(() => {
     const _cartItems = getCartItems();
 
-    console.log(_cartItems);
-    
     setCartItems(_cartItems);
   }, []);
 
@@ -31,37 +30,55 @@ const Cart = (_props: Props): React.ReactElement => {
     }
   }
 
+
+  const renderCartEmpty = () => {
+    return (
+      <Stacked>
+        <Subheading>Cart Empty</Subheading>
+        <p>you should add some validators to nominate...</p>
+        <Button onClick={() => navigate('/validators')}>Take Me There!</Button>
+      </Stacked>
+    )
+  }
+
   return (
     <Container>
       <Grid stretched>
         <Grid.Row>
-          <Subheading>Your Cart:</Subheading>
+          <Grid.Column floated='left' width='6'>
+            <StackedHorizontal justifyContent='space-between'>
+              <Subheading>Your Cart:</Subheading>
+              <Button appearance='outline' onClick={clearCart} size='tiny'>Clear</Button>
+            </StackedHorizontal>
+          </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width='6'>
             <List animated relaxed>
               {
-                cartItems && cartItems.map((item: string) => {
-                  const address = stripAddressFromCartItem(item);
+                cartItems.length
+                  ? cartItems.map((item: string) => {
+                      const address = stripAddressFromCartItem(item);
 
-                  return (
-                    <List.Item>
-                    <WithSpaceAround>
-                      <StackedHorizontal>
-                        <AddressSummary address={address} noPlaceholderName orientation='horizontal' size='small' />
-                        <Margin left />
-                        <Icon name='close' link onClick={removeItemFromCart} data-key={item}/>
-                        {/* TODO: <NominationDetails address={address} /> */}
-                      </StackedHorizontal>
-                    </WithSpaceAround>
-                  </List.Item>
-                  )
-                })
+                      return (
+                        <List.Item>
+                        <WithSpaceAround>
+                          <StackedHorizontal>
+                            <AddressSummary address={address} noPlaceholderName orientation='horizontal' size='small' />
+                            <Margin left />
+                            <Icon name='close' link onClick={removeItemFromCart} data-key={item}/>
+                            {/* TODO: <NominationDetails address={address} /> */}
+                          </StackedHorizontal>
+                        </WithSpaceAround>
+                      </List.Item>
+                      )
+                    })
+                  : renderCartEmpty()
               }
             </List>
           </Grid.Column>
           <Grid.Column width='4' floated='right'>
-            <Button>
+            <Button disabled={!cartItems.length}>
               Checkout
             </Button>
           </Grid.Column>
