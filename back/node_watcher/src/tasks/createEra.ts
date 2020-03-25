cc121f58d70970a1ad8ba5d48df6600e84a59cc5
YJ <yjkimjunior@gmail.com>
2020-03-25 13:42:52 +0800

    fix: uncomment nomidot tasks (#252)
    * fix: uncomment session
    
    * fix: uncomment recent offline
    
    * fix: rewards
    
    * fix: era
    
    * fix: slashing
    
    * fix: slashing
    
    * fix: staske and nominations
    
    * fix: era old
    
    * fix: old stakers
    
    * fix: old total stake
    
    * fix: lint
    
    * fix: new job fill in commented stuff
    
    * fix: unwrapordefault
    
    * fix: jobs

--- back/node_watcher/src/tasks/createEra.ts
+++ back/node_watcher/src/tasks/createEra.ts
@@ -3,7 +3,14 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { ApiPromise } from '@polkadot/api';
-import { BlockNumber, Hash } from '@polkadot/types/interfaces';
+import { Option } from '@polkadot/types';
+import {
+  BlockNumber,
+  EraPoints,
+  EraRewardPoints,
+  Hash,
+  SessionIndex,
+} from '@polkadot/types/interfaces';
 import { logger } from '@polkadot/util';
 
 import { prisma } from '../generated/prisma-client';
@@ -22,16 +29,32 @@ const createEra: Task<NomidotEra> = {
     api: ApiPromise
   ): Promise<NomidotEra> => {
     const idx = await api.query.staking.currentEra.at(blockHash);
-    const points = await api.query.staking.currentEraPointsEarned.at(blockHash);
-    const currentEraStartSessionIndex = await api.query.staking.currentEraStartSessionIndex.at(
-      blockHash
-    );
+    let points;
+    let currentEraStartSessionIndex;
+    let result = {} as NomidotEra;
 
-    const result = {
-      idx,
-      points: api.createType('EraPoints', points),
-      startSessionIndex: api.createType('Index', currentEraStartSessionIndex),
-    };
+    if (api.query.staking.currentEraStartSessionIndex) {
+      points = await api.query.staking.currentEraPointsEarned.at<EraPoints>(
+        blockHash
+      );
+      currentEraStartSessionIndex = await api.query.staking.currentEraStartSessionIndex.at<
+        SessionIndex
+      >(blockHash);
+    } else {
+      points = await api.query.staking.erasRewardPoints.at<EraRewardPoints>(
+        blockHash,
+        idx.unwrapOrDefault()
+      );
+      currentEraStartSessionIndex = await api.query.staking.erasStartSessionIndex.at<
+        Option<SessionIndex>
+      >(blockHash, idx.unwrapOrDefault());
+
+      result = {
+        idx,
+        points,
+        startSessionIndex: currentEraStartSessionIndex.unwrapOrDefault(),
+      };
+    }
 
     l.log(`NomidotEra: ${JSON.stringify(result)}`);
 
@@ -46,28 +69,28 @@ const createEra: Task<NomidotEra> = {
 
     // check if record exists
     const eraIndexAlreadyExists = await prisma.$exists.era({
-      index: idx.unwrap().toNumber(),
+      index: idx.unwrapOrDefault().toNumber(),
     });
 
     if (eraIndexAlreadyExists) {
       await prisma.updateEra({
         data: {
           individualPoints: {
-            set: points.individual.map(points => points.toHex()),
+            set: points.individual.toHex(),
           },
           totalPoints: points.total.toHex(),
         },
         where: {
-          index: idx.unwrap().toNumber(),
+          index: idx.unwrapOrDefault().toNumber(),
         },
       });
     } else if (startSessionIndex.toNumber() > 0) {
       // only start writing after there's actually been a session.
       await prisma.createEra({
-        index: idx.unwrap().toNumber(),
+        index: idx.unwrapOrDefault().toNumber(),
         totalPoints: points.total.toHex(),
         individualPoints: {
-          set: points.individual.map(points => points.toHex()),
+          set: points.individual.toHex(),
         },
         eraStartSessionIndex: {
           connect: {
