02147bcb953c386a80f318049d29f786da2dc343
YJ <yjkimjunior@gmail.com>
2020-03-30 22:49:37 +0800

    feat: new header

--- front/gatsby/src/components/Layout/Header/Header.tsx
+++ front/gatsby/src/components/Layout/Header/Header.tsx
@@ -3,12 +3,10 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { RouteComponentProps } from '@reach/router';
-import { Button, MainMenu } from '@substrate/design-system';
 import { useLocalStorage } from '@substrate/local-storage';
-import { Container, Icon, StackedHorizontal } from '@substrate/ui-components';
-import { navigate } from 'gatsby';
+import { Icon, Menu } from '@substrate/ui-components';
+import { Link, navigate } from 'gatsby';
 import React from 'react';
-import shortid from 'shortid';
 
 import { APP_TITLE } from '../../../util';
 import { AccountsDropdown } from '../../AccountsDropdown';
@@ -30,42 +28,30 @@ export default function Header(_props: Props): React.ReactElement {
 
   return (
     <>
-      <MainMenu
-        contentLeft={<h2>{APP_TITLE}</h2>}
-        contentRight={
-          <StackedHorizontal justifyContent='center' alignItems='center'>
-            <AccountsDropdown />
-            <Icon
-              inverted
-              link
-              name='cart'
-              size='large'
-              onClick={navToCartPage}
-            />
+      <Menu>
+        <Menu.Item>
+          <h2>{APP_TITLE}</h2>
+        </Menu.Item>
+        <Menu.Menu>
+          <Menu.Item>
+            <Link to='/accounts'>Accounts</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to='/validators'>Validators</Link>
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Menu position='right'>
+          <BlockHeader />
+          <EraHeader />
+          <SessionHeader />
+          <StakingHeader />
+          <AccountsDropdown />
+          <Menu.Item>
+            <Icon link name='cart' size='large' onClick={navToCartPage} />
             <p>{cartItemsCount}</p>
-          </StackedHorizontal>
-        }
-        tabs={[
-          <Button
-            onClick={(): Promise<void> => navigate('/accounts')}
-            key={shortid.generate()} // FIXME: why do i need a key here...
-          >
-            Accounts
-          </Button>,
-          <Button
-            onClick={(): Promise<void> => navigate('/validators')}
-            key={shortid.generate()} // FIXME: why do i need a key here>
-          >
-            Validators
-          </Button>,
-        ]}
-      />
-      <Container style={{ display: 'flex' }}>
-        <BlockHeader />
-        <EraHeader />
-        <SessionHeader />
-        <StakingHeader />
-      </Container>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     </>
   );
 }
